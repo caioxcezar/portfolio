046b741da43e3f91b7ff34cef7b556f51dd30bce
Caio Rezende <caiocezar50@outlook.com>
2024-08-24 16:35:44 -0300

    feat: responsive and links for the projects

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -49,33 +49,34 @@ export default function Home() {
     window.open(url);
   };
 
-  const openLinkedIn = () =>
-    window.open("https://www.linkedin.com/in/caiocsrezende/");
+  const openLink = (link) => window.open(link);
 
   return (
-    <main className="body grid grid-cols-5 dark:bg-gray-950 bg-gray-400 w-100 h-dvh select-none">
-      <div className="flex flex-col shadow-gray-900 shadow-ml dark:bg-black/50 bg-gray-50/75 text-gray-900 dark:text-gray-50 rounded-2xl m-4">
-        <div className="mx-4 mt-4 font-bold">
-          <div className="text-4xl italic absolute">Caio</div>
-          <div
-            className={`${pressStartFont} text-4xl text-gray-500 text-end w-100 pt-7`}
-          >
-            Rezende
+    <main className="body dark:bg-gray-950 bg-gray-400 w-100 h-full select-none flex flex-row">
+      <div className="hidden md:block md:w-1/3 lg:block lg:w-1/4">
+        <div className="flex flex-col shadow-gray-900 shadow-ml dark:bg-black/50 bg-gray-50/75 text-gray-900 dark:text-gray-50 h-full">
+          <div className="mx-4 mt-4 font-bold">
+            <div className="text-4xl italic absolute">Caio</div>
+            <div
+              className={`${pressStartFont} text-4xl text-gray-500 text-end w-100 pt-7`}
+            >
+              Rezende
+            </div>
           </div>
+          <ul className="nav py-4 flex-1 md:w-2/3 lg:w-3/4">
+            <FlatList
+              items={menus}
+              keyExtractor={(item) => item.target}
+              renderItem={(item) => (
+                <NavItem {...item} selected={hash == item.target} />
+              )}
+            />
+          </ul>
+          <Coffee className="mb-2" />
         </div>
-        <ul className="nav py-4 flex-1">
-          <FlatList
-            items={menus}
-            keyExtractor={(item) => item.target}
-            renderItem={(item) => (
-              <NavItem {...item} selected={hash == item.target} />
-            )}
-          />
-        </ul>
-        <Coffee className="mb-4" />
       </div>
-      <div className="col-span-4">
-        <div className="container mx-auto px-4 h-screen overflow-x-auto pb-4">
+      <div className="w-full">
+        <div className="mx-auto px-4 h-screen overflow-x-auto pb-4">
           <div className="mb-4">
             <TopIcon
               icon={theme.value == "dark" ? "icon-moon-inv" : "icon-sun-inv"}
@@ -84,7 +85,7 @@ export default function Home() {
             />
             <TopIcon
               icon={"icon-linkedin-squared"}
-              onclick={openLinkedIn}
+              onclick={openLink("https://www.linkedin.com/in/caiocsrezende/")}
               className="me-2"
             />
             <TopIcon
@@ -92,7 +93,7 @@ export default function Home() {
               onclick={() => window.open("https://github.com/caioxcezar")}
             />
           </div>
-          <div className="flex flex gap-4 flex-col">
+          <div className="flex gap-4 flex-col">
             <Card id={about.id} image={about.image} title={about.title}>
               <>
                 <div className="flex gap-2">
@@ -109,20 +110,28 @@ export default function Home() {
               <FlatList
                 items={portfolio.items}
                 keyExtractor={(item) => item.title}
-                renderItem={(item) => (
+                renderItem={({ title, subtitle, description, link, image }) => (
                   <div className="flex">
-                    {item.image && (
+                    {image && (
                       <Image
-                        src={item.image}
-                        width={500}
-                        height={500}
+                        src={image}
+                        width={250}
+                        height={250}
                         alt="Image of the project"
                       />
                     )}
                     <div className="pl-4 py-4">
-                      <p className="text-xl font-bold">{item.title}</p>
-                      <p className="text-lg">{item.subtitle}</p>
-                      <p className="text-base">{item.description}</p>
+                      <div className="h-full">
+                        <div className="h-3/4">
+                          <p className="text-xl font-bold">{title}</p>
+                          <p className="text-lg">{subtitle}</p>
+                          <p className="text-base">{description}</p>
+                        </div>
+                        <Button
+                          title={"Open Project"}
+                          onclick={() => openLink(link)}
+                        />
+                      </div>
                     </div>
                   </div>
                 )}
@@ -220,7 +229,9 @@ export default function Home() {
                 )}
               />
               <FlatList
-                className={"grid grid-cols-4 gap-4 mt-2"}
+                className={
+                  "grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 mt-2"
+                }
                 items={education.certifications}
                 keyExtractor={(item) => item.title}
                 renderItem={({ title, description, date }) => (
@@ -237,7 +248,12 @@ export default function Home() {
             <Card id={contact.id} title={contact.title}>
               <span className="text-xl">
                 Send a e-mail or message via{" "}
-                <Button title={"LinkedIn"} onclick={openLinkedIn} />
+                <Button
+                  title={"LinkedIn"}
+                  onclick={() =>
+                    openLink("https://www.linkedin.com/in/caiocsrezende/")
+                  }
+                />
               </span>
               <Input
                 error={subjectError}
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,8 @@
-const Button = ({ onclick, title }) => {
+const Button = ({ onclick, title, className = "" }) => {
+  const _className =
+    "bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-300 p-2 rounded-full text-white";
   return (
-    <button
-      className="bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-300 p-2 rounded-full text-white"
-      onClick={() => onclick()}
-    >
+    <button className={`${_className} ${className}`} onClick={onclick}>
       {title}
     </button>
   );
